1565ed5934cc07a9af29632e8b73cfb0d8ec7873
KaelDucatti <ducatti860@gmail.com>
2025-07-09 20:37:36 -0300

    criando método POST para funçao 'new_user_view'

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def new_user_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(
+                request,
+                template_name="new_user_success.html",
+                context={"user_data": form},
+            )
+    else:
+        form = UserCreationForm()
+    return render(
+        request, template_name="new_user.html", context={"new_user_form": form}
+    )
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from projeto-carros!")
+
+
+if __name__ == "__main__":
+    main()
