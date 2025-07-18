bed44a3ad313df1eacef3b49c7a766481274ffa4
KaelDucatti <ducatti860@gmail.com>
2025-07-18 18:08:19 -0300

    refatorando signals: 'car_post_save' e 'car_post_delete'

--- cars/signals.py
+++ cars/signals.py
@@ -5,15 +5,17 @@ from django.dispatch import receiver
 from .models import Car, CarInventory
 
 
-@receiver(post_save, sender=Car)
-def car_post_save(sender, instance, **kwargs):
+def car_invenroty_update():
     cars_count = Car.objects.count()
     cars_value = Car.objects.aggregate(Sum("price")).get("price__sum")
     CarInventory.objects.create(cars_count=cars_count or 0, cars_value=cars_value or 0)
 
 
+@receiver(post_save, sender=Car)
+def car_post_save(sender, instance, **kwargs):
+    car_invenroty_update()
+
+
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
-    cars_count = Car.objects.count()
-    cars_value = Car.objects.aggregate(total_value=Sum("price")).get("total_value")
-    CarInventory.objects.create(cars_count=cars_count or 0, cars_value=cars_value or 0)
+    car_invenroty_update()
